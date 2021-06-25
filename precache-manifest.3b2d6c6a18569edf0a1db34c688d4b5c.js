758013e3633015ec08be58eb55d72de992067b64
Ratriel <55146317+Ratriel@users.noreply.github.com>
2021-06-25 07:49:23 -0600

    Updates

---
+++ precache-manifest.3b2d6c6a18569edf0a1db34c688d4b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09bd6cb63dc93d4a810cfc9c58ba4f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8f224bc308704f538e6",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "d27b368ceaa0d134b3f1",
+    "url": "./static/js/2.d7333729.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.d7333729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f224bc308704f538e6",
+    "url": "./static/js/main.2e3aa5f2.chunk.js"
+  },
+  {
+    "revision": "cec5e865e1e494dd04ee",
+    "url": "./static/js/runtime-main.9a589eb2.js"
+  }
+]);
